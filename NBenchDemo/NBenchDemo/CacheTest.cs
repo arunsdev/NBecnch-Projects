0264d91eceb3eaba0ce61ce34f9ed169a4f7dd66
archandran <arunchandranc@ymail.com>
2019-03-14 12:33:31 +0530

    files added
    files added

---
+++ NBenchDemo/NBenchDemo/CacheTest.cs
@@ -0,0 +1,43 @@
+﻿using NBench;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBenchDemo
+{
+    public class SimpleCache<T>
+    {
+        private readonly List<T> cache = new List<T>();
+
+        public void Add(T item)
+        {
+            if (!Contains(item))
+                cache.Add(item);
+        }
+
+        public bool Contains(T item)
+        {
+            return cache.Contains(item);
+        }
+    }
+    
+   public class CacheTest
+    {
+        private SimpleCache<string> cache = new SimpleCache<string>();
+
+        [PerfBenchmark(NumberOfIterations = 1,
+           RunMode = RunMode.Throughput,
+           TestMode = TestMode.Test,
+           SkipWarmups = true)]
+        [ElapsedTimeAssertion(MaxTimeMilliseconds = 2000)]
+        public void Add_Benchmark_Performance()
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                cache.Add(i.ToString());
+            }
+        }
+    }
+}
---
+++ NBenchDemo/NBenchDemo/CounterPerfSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBench;
+namespace NBenchDemo
+{
+   public class CounterPerfSpecs
+    {
+        private Counter _counter;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _counter = context.GetCounter("TestCounter");
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+            NumberOfIterations = 3, RunMode = RunMode.Throughput,
+            RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 10000000.0d)]
+        [MemoryAssertion(MemoryMetric.TotalBytesAllocated, MustBe.LessThanOrEqualTo, ByteConstants.ThirtyTwoKb)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void Benchmark()
+        {
+            _counter.Increment();
+        }
+
+        [PerfCleanup]
+        public void Cleanup()
+        {
+            // does nothing
+        }
+    }
+}
